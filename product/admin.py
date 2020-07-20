7aa241132d7aad35e507803e7c35d359166fac0d
Armaan <armaansheikh65@gmail.com>
2020-07-20 11:36:51 +0530

    new models added

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Item, Cart, Order
+from .models import Item, Cart, Order, Rating
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(Cart)
 admin.site.register(Order)
+admin.site.register(Rating)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
@@ -15,6 +16,20 @@ class Item(models.Model):
     description = models.TextField()
     image = models.ImageField('items')
     discount_price = models.FloatField(blank=True, null=True)
+
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(item=self)
+        return len(ratings)
+    
+    def avg_rating(self):
+        ratings = Rating.objects.filter(item=self)
+        sum=0
+        for rating in ratings:
+            sum += rating.rating
+        if len(ratings)>0:
+            return sum/len(ratings)
+        else:
+            return None
     
 
     def __str__(self):
@@ -22,7 +37,7 @@ class Item(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE)
-    item = models.ForeignKey(Item, on_delete= models.CASCADE)
+    item = models.ForeignKey(Item, related_name='cart_item', on_delete= models.CASCADE)
     ordered = models.BooleanField(default=False)
     quantity = models.IntegerField(default=1)
 
@@ -55,3 +70,13 @@ class Order(models.Model):
     def __str__(self):
         return self.user.username
 
+class Rating(models.Model):
+    item = models.ForeignKey(Item, related_name='item', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    class Meta:
+        unique_together = (('user', 'item'),)
+        index_together = (('user', 'item'),)
+
+    def __str__(self):
+        return self.item.title
