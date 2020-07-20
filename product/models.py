4ec86db07d9dc18b8049dc7eb72b4bd1343bf89f
Armaan <armaansheikh65@gmail.com>
2020-07-20 11:27:57 +0530

    models added

--- product/models.py
+++ product/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('J', 'Jeans'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Outwear'),
+)
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    description = models.TextField()
+    image = models.ImageField('items')
+    discount_price = models.FloatField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    item = models.ForeignKey(Item, on_delete= models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
+
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
+    def __str__(self):
+        return self.item.title
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    items = models.ManyToManyField(Cart)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    shipping_address = models.CharField(max_length=400)
+    billing_address = models.CharField(max_length=400)
+    received = models.BooleanField(default=False)
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+
+    def __str__(self):
+        return self.user.username
+
